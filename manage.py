5423745001e0ddd40ecff60deb0add31f449c3dc
Donna <skies09@hotmail.com>
2025-06-15 16:33:14 +0100

    add debug

--- manage.py
+++ manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+import sys
+
+print(sys.version)
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuroverse.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neuroverse.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
