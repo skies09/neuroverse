a1fa8793bdec79adbacd715556c0a2e66cb7b842
Donna <skies09@hotmail.com>
2025-06-15 15:04:36 +0100

    initial commit

---
+++ neuroverse/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for neuroverse project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuroverse.settings')
+
+application = get_asgi_application()
